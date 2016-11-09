20becd584699f78030a3edf616987dd0d42569f1
seconoid <seconsecond@gmail.com>
2016-11-09 17:32:57 +0900

    学生登録フォームの作成

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,9 @@
 class StudentsController < ApplicationController
+  def show
+    @student = Student.find([:id])
+  end
+
   def new
+    @student = Student.new
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: 'home#index'
   get 'signup' => 'students#new'
+  resources :students
 end
