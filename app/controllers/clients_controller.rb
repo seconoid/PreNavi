a152693ba2b4ee9d0bab38068fb8cf33323ed98f
seconoid <seconsecond@gmail.com>
2017-01-10 11:09:35 +0900

    クライアントのログイン機能を追加

---
+++ app/controllers/clients_controller.rb
@@ -0,0 +1,26 @@
+class ClientsController < ApplicationController
+  def show
+    @client = Client.find(params[:format])
+  end
+
+  def new
+    @client = Client.new
+  end
+
+  def create
+    @client = Client.new(client_params)
+    if @client.save
+      log_in @client
+      flash[:success] = "Prenaviにようこそ！"
+      redirect_to @client
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def client_params
+      params.require(:client).permit(:name, :email, :password)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
   get 'students' => 'students#index'
+  get 'signup_client' => 'clients#new'
+  get 'profile' => 'clients#show'
   resources :students
+  resource :client
 end
