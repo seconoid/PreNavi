1863324d4412597a85dbe7194d0e687d839b0893
seconoid <seconsecond@gmail.com>
2017-01-12 10:06:32 +0900

    デフォルトアイコンの追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,18 +8,36 @@
 
 
 # student seeds
+name = "中山暁登"
+s_class = "IH13A296"
+s_code = 40595
+s_no = 27
+password = "mogemoge"
+image = open("#{Rails.root}/public/icon_default.png")
+Student.create!(
+  name: name,
+  s_class: s_class,
+  s_code: s_code,
+  s_no: s_no,
+  password: password,
+  image: image
+)
+
 30.times do |n|
   name = Faker::Name.name
   s_class = "XX99X999"
   s_code = 90000 + n
   s_no = 99
   password = "foobar"
+  image = open("#{Rails.root}/public/icon_default.png")
   Student.create!(
     name: name,
     s_class: s_class,
     s_code: s_code,
     s_no: s_no,
-    password: password)
+    password: password,
+    image: image
+  )
 end
 
 # client seeds
