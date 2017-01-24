5e047d1860669e7ce573597059c59b2957311402
seconoid <seconsecond@gmail.com>
2017-01-24 13:59:09 +0900

    新規登録画面を統合

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   root to: 'home#index'
   get 'signup' => 'users#new'
-  get 'student_signup' => 'students#new'
-  get 'client_signup' => 'client#new'
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
