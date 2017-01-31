73588c80b10b2b93c214c0ccd6c57e6065644f5f
seconoid <seconsecond@gmail.com>
2017-01-31 15:00:06 +0900

    ユーザのテストデータを変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-# student seeds
+# student seed
 name = "中山暁登"
 email = "akito@example.com"
 s_class = "IH13A296"
@@ -29,7 +29,7 @@ User.create!(
   user_attr: user_attr
 )
 
-# client seeds
+# client seed
 name = "熊谷基継"
 email = "kumamoto@example.com"
 c_id = "hal_tokyo"
@@ -49,41 +49,59 @@ User.create!(
   user_attr: user_attr
 )
 
-# 30.times do |n|
-#   name = Faker::Name.name
-#   s_class = "XX99X999"
-#   s_code = 90000 + n
-#   s_no = 99
-#   password = "foobar"
-#   image = open("#{Rails.root}/public/icon_default.png")
-#   Student.create!(
-#     name: name,
-#     s_class: s_class,
-#     s_code: s_code,
-#     s_no: s_no,
-#     password: password,
-#     image: image
-#   )
-# end
+# product seed
+Product.create!(
+  student_id: 1,
+  name: "Prenavi",
+  about: "就活プレゼンテーションでの学生と企業のやりとりをよりよくするためのアプリケーションです。",
+  url: "http://localhost:3000"
+)
 
-# # product seeds
-# Product.create!(
-#   student_id: 1,
-#   name: "Prenavi",
-#   about: "就活プレゼンテーションでの学生と企業のやりとりをよりよくするためのアプリケーションです。",
-#   url: "http://localhost:3000"
-# )
+# student fakers
+30.times do |n|
+  name = Faker::Name.name
+  email = Faker::Internet.email
+  s_class = "XX99X999"
+  s_code = 90000 + n
+  s_no = 99
+  password = "foobar"
+  image = open("#{Rails.root}/public/icon_default.png")
+  user_attr = 1
+  User.create!(
+  name: name,
+  email: email,
+  password: password,
+  image: image,
+  student_attributes: {
+    s_class: s_class,
+    s_code: s_code,
+    s_no: s_no
+  },
+  user_attr: user_attr
+)
+end
 
-# # client seeds
-# 10.times do |n|
-#   name = Faker::Name.name
-#   email = "hogehoge@example.com"
-#   password = "mogemoge"
-#   Client.create!(
-#     name: name,
-#     email: email,
-#     password: password)
-# end
+# client fakers
+10.times do |n|
+  name = Faker::Name.name
+  email = Faker::Internet.email
+  c_id = "hal_tokyo"
+  c_name = "HAL東京"
+  password = "foobar"
+  image = open("#{Rails.root}/public/icon_default.png")
+  user_attr = 2
+  User.create!(
+  name: name,
+  email: email,
+  password: password,
+  image: image,
+  client_attributes: {
+    c_id: c_id,
+    c_name: c_name
+  },
+  user_attr: user_attr
+)
+end
 
 # # favorite seeds
 # Favorite.create!(student_id: 1, client_id: 1)
