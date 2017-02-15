f8de125181fba6ac0d3dfc3b3a2fa912f9855f56
seconoid <seconsecond@gmail.com>
2017-02-15 10:34:29 +0900

    31階のデータを追加

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -56,24 +56,59 @@ class RoomsController < ApplicationController
           y: 0
         },
         {
+          exhibit_code: "IT101",
+          x: 12,
+          y: 0
+        },
+        {
+          exhibit_code: "IT102",
+          x: 13,
+          y: 0
+        },
+        {
+          exhibit_code: "IT103",
+          x: 15,
+          y: 0
+        },
+        {
+          exhibit_code: "IT104",
+          x: 16,
+          y: 0
+        },
+        {
+          exhibit_code: "IT108",
+          x: 18,
+          y: 0
+        },
+        {
+          exhibit_code: "IT109",
+          x: 19,
+          y: 0
+        },
+        {
+          exhibit_code: "IT110",
+          x: 21,
+          y: 0
+        },
+        {
+          exhibit_code: "IT111",
+          x: 22,
+          y: 0
+        },
+        {
           exhibit_code: "IT049",
           x: 0,
+          y: 1
+        },
+        {
+          exhibit_code: "IT112",
+          x: 23,
           y: 1
         },
         {
           exhibit_code: "IT145",
           x: 0,
           y: 2
-        },
-        {
-          exhibit_code: "IT144",
-          x: 0,
-          y: 4
-        },
-        {
-          exhibit_code: "IT143",
-          x: 0,
-          y: 5
         },
         {
           exhibit_code: "IT047",
@@ -86,14 +121,164 @@ class RoomsController < ApplicationController
           y: 2
         },
         {
+          exhibit_code: "IT007",
+          x: 11,
+          y: 2
+        },
+        {
+          exhibit_code: "IT014",
+          x: 13,
+          y: 2
+        },
+        {
+          exhibit_code: "IT071",
+          x: 17,
+          y: 2
+        },
+        {
+          exhibit_code: "IT203",
+          x: 19,
+          y: 2
+        },
+        {
+          exhibit_code: "IT113",
+          x: 23,
+          y: 2
+        },
+        {
+          exhibit_code: "IT144",
+          x: 0,
+          y: 4
+        },
+        {
           exhibit_code: "IT045",
           x: 5,
           y: 4
         },
         {
           exhibit_code: "IT046",
-          x: 5,
+          x: 7,
+          y: 4
+        },
+        {
+          exhibit_code: "IT005",
+          x: 11,
+          y: 4
+        },
+        {
+          exhibit_code: "IT006",
+          x: 13,
+          y: 4
+        },
+        {
+          exhibit_code: "IT008",
+          x: 17,
+          y: 4
+        },
+        {
+          exhibit_code: "IT202",
+          x: 19,
+          y: 4
+        },
+        {
+          exhibit_code: "IT114",
+          x: 23,
+          y: 4
+        },
+        {
+          exhibit_code: "IT143",
+          x: 0,
+          y: 5
+        },
+        {
+          exhibit_code: "IT115",
+          x: 23,
+          y: 5
+        },
+        {
+          exhibit_code: "IT043",
+          x: 4,
+          y: 6
+        },
+        {
+          exhibit_code: "IT051",
+          x: 6,
+          y: 6
+        },
+        {
+          exhibit_code: "IT003",
+          x: 10,
+          y: 6
+        },
+        {
+          exhibit_code: "IT004",
+          x: 12,
+          y: 6
+        },
+        {
+          exhibit_code: "IT107",
+          x: 16,
+          y: 6
+        },
+        {
+          exhibit_code: "IT201",
+          x: 18,
+          y: 6
+        },
+        {
+          exhibit_code: "IT142",
+          x: 0,
           y: 7
+        },
+        {
+          exhibit_code: "IT601",
+          x: 17,
+          y: 7
+        },
+        {
+          exhibit_code: "IT116",
+          x: 23,
+          y: 7
+        },
+        {
+          exhibit_code: "IT141",
+          x: 0,
+          y: 8
+        },
+        {
+          exhibit_code: "IT041",
+          x: 4,
+          y: 8
+        },
+        {
+          exhibit_code: "IT042",
+          x: 6,
+          y: 8
+        },
+        {
+          exhibit_code: "IT001",
+          x: 10,
+          y: 8
+        },
+        {
+          exhibit_code: "IT002",
+          x: 12,
+          y: 8
+        },
+        {
+          exhibit_code: "IT105",
+          x: 16,
+          y: 8
+        },
+        {
+          exhibit_code: "IT106",
+          x: 18,
+          y: 8
+        },
+        {
+          exhibit_code: "IT117",
+          x: 23,
+          y: 8
         },
       ]
     end
